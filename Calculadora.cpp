62eaacf4032b7e012645a230597c572e428fea57
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-01-31 01:22:30 -0600

    suma

---
+++ Calculadora.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Valor1, Valor2, resultado;
+    cout << "Ingresa el primer valor que deseas sumar:";
+    cin >> Valor1;
+    cout << endl <<"Ingresa el segundo valor que deseas sumar:";
+    cin >> Valor2;
+    resultado= Valor1 + Valor2;
+    cout << endl <<"La suma de los valores ingresados es:" << resultado;
+
+
+    return 0;
+}
