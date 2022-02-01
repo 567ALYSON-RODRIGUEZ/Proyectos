360f0109f40e119d8ebedb435ce426ac72a9c303
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-01-31 18:29:17 -0600

    Operaciones restantes

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -13,5 +13,32 @@ int main()
     cout << endl <<"La suma de los valores ingresados es:" << resultado;
 
 
+
+    cout << endl << endl << "Ingresa el primer valor que deseas restar:";
+    cin >> Valor1;
+    cout << endl <<"Ingresa el segundo valor que deseas restar:";
+    cin >> Valor2;
+    resultado= Valor1 - Valor2;
+    cout << endl <<"La restar de los valores ingresados es:" << resultado;
+
+
+
+    cout << endl << endl << "Ingresa el primer valor que deseas multiplicar:";
+    cin >> Valor1;
+    cout << endl <<"Ingresa el segundo valor que deseas multiplicar:";
+    cin >> Valor2;
+    resultado= Valor1 * Valor2;
+    cout << endl <<"La multiplicación de los valores ingresados es:" << resultado;
+
+
+
+    cout << endl << endl << "Ingresa el primer valor que deseas dividir:";
+    cin >> Valor1;
+    cout << endl <<"Ingresa el segundo valor que deseas dividir:";
+    cin >> Valor2;
+    resultado= Valor1 / Valor2;
+    cout << endl <<"La division de los valores ingresados es:" << resultado;
+
+
     return 0;
 }
