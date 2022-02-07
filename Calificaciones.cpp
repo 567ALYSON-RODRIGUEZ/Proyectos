70d17af9fd72b372efce857651b67d5f958065db
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-02-06 23:24:23 -0600

    Indice de los cursos

--- Calificaciones.cpp
+++ Calificaciones.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 int main()
 {
-    int carne,primerP,segundoP,zona,finalP,Calificacionfinal;
+    int op,carne,primerP,segundoP,zona,finalP,Calificacionfinal;
     string nombreC;
 
 
     cout << "********Calificaciones obtenidas en el semestre********";
     cout << endl << "Ingrese su nombre completo:";
     cin >> nombreC;
-    cout << "Ingrese su no.de Carné:";
+    cout << "Ingrese su no.de Carne:";
     cin >> carne;
-    cout << "Calificaciones del estudiante" << nombreC << " " << carne;
-
+    cout << "Calificaciones del estudiante" << " " << nombreC << " " << carne << endl;
+    cout << endl << "*************Ingrese el codigo del curso***************" << endl;
+    printf("011.Fisica I\n");
+    printf("012.Progrmacion I\n");
+    printf("013.Calculo\n");
+    printf("014.Proceso Administrativo\n");
+    printf("015.Derecho Informatico\n");
+    scanf("%d", &op);
 }
