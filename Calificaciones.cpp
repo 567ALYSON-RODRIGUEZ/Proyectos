9a47a31a303dd06cb1c2678a08c594878b9c7d16
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-02-06 22:33:43 -0600

    Datos del estudiante

---
+++ Calificaciones.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int carne,primerP,segundoP,zona,finalP,Calificacionfinal;
+    string nombreC;
+
+
+    cout << "********Calificaciones obtenidas en el semestre********";
+    cout << endl << "Ingrese su nombre completo:";
+    cin >> nombreC;
+    cout << "Ingrese su no.de Carné:";
+    cin >> carne;
+    cout << "Calificaciones del estudiante" << nombreC << " " << carne;
+
+}
