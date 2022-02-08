128a34bf159a5020f9ee7bc02fb051bf4e87ad96
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-02-07 22:43:37 -0600

    datos del estudiante

---
+++ notas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opc,carne,primerP,segundoP,zona,finalP;
+    string nombreC;
+
+
+    cout << "********Calificaciones obtenidas en el semestre********";
+    cout << endl << "Ingrese su nombre:";
+    cin >> nombreC;
+    cout << "Ingrese su no.de Carne:";
+    cin >> carne;
+}
