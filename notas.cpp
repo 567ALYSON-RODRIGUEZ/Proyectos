3d707276cca22ffb899d555b361f0f33ae7871bf
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-02-07 23:15:55 -0600

    Agregar opcion de curso Programacion I

--- notas.cpp
+++ notas.cpp
@@ -56,4 +56,37 @@ int main()
                         }
 
     }
+    switch (opc)
+    {
+        case 2:
+            cout<< endl<< "Ha ingresado al curso de Programacion I";
+            cout<< endl<< "Ingrese la nota obtenida en actividades:" << endl;
+            cin >> zona;
+            cout<< "Ingrese la nota obtenida en el Parcial I:"<< endl;
+            cin >> primerP;
+            cout<< "Ingrese la nota obtenida en el Parcial II:"<< endl;
+            cin>> segundoP;
+            int Calificacionfinal= zona+primerP+segundoP;
+            cout<< "Su zona es de:"<< Calificacionfinal<< endl;
+            if (Calificacionfinal <35)
+            {cout<< "No ha alcanzado la zona minima para examen final :("<< endl;}
+            else
+                if(Calificacionfinal >65)
+                    {cout<< "Error, escriba nuevamente las notas";}
+                else
+                    if(Calificacionfinal >=35)
+                        {cout<< "Ha alcanzado la zona necesaria para realizar examen final, felicidades :D";
+                        cout<<endl<< "Ingrese la nota obtenida en examen final:"<< endl;
+                         cin>> finalP;
+                         int nota_final= Calificacionfinal+finalP;
+                            if ( Calificacionfinal <=35 && Calificacionfinal !=0 && finalP <=10 && finalP !=0)
+                            cout<< "Su nota final del curso es:"<< " "<< nota_final<< " "<< "ha PERDIDO el curso de Programacion I, se gana con 65";
+                                else
+                                    if(Calificacionfinal >35 && Calificacionfinal <=39 && finalP <25 && finalP !=0)
+                                    cout<< "Su nota final del curso es:"<< " "<< nota_final<< " "<< "ha PERDIDO el curso de Programacion I, se gana con 65";
+                                        else
+                                            if (Calificacionfinal >=40 && Calificacionfinal <=65 && finalP >=25 && finalP <=30 && finalP !=0)
+                                              cout<< "Su nota final del curso es:"<< " "<< nota_final<< " "<< "ha GANADO el curso de Programacion I,felicidades";
+                        }
+    }
 }
