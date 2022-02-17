a17909a0fd1509525f8132b35819913fcaf68168
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-02-17 02:40:49 -0600

    Limites del robot

---
+++ tarea2/Calculadora de funciones.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+
+using namespace std;
+
+int sumaN(int num1, int num2){
+    return num1 + num2;}
+
+int restaN(int num1, int num2){
+    return num1 - num2;}
+
+int multiN(int num1, int num2){
+    return num1 * num2;}
+
+int divN(int num1, int num2){
+    return num1 / num2;}
+
+int main()
+{
+
+	return 0;
+}
---
+++ tarea3/Juego-robot.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+void drawMap(int posX,int posY,char gameMap[10][10]){
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      if(posX==j && posY==i)
+        {
+            cout<<"R";
+        }
+      else
+      {
+            cout<<gameMap[i][j];
+      }
+    }
+    cout<<endl;
+  }
+}
+int main(){
+  int posX=3;
+  int posY=3;
+  char map[10][10]={{'0','0','0','0','0','0','0','0','0','0'},
+                    {'0','0','0','0','0','0','0','0','0','0'},
+                    {'0','0','0','0','0','0','0','0','0','0'},
+                    {'0','0','0','0','0','0','0','0','0','0'},
+                    {'0','0','0','0','0','0','0','0','0','0'}};
+  char teclado;
+  bool gameOver= false;
+  drawMap(posX,posY,map);
+  while(!gameOver)
+  {
+    cout << "Bienvenido(a)"  << endl;
+    cout << "Controles Teclas --> w(arriba), a(izquierda), d(derecha) y s(abajo)" << endl;
+    cout << "Controles Teclas --> p(Salida)" << endl;
+    cin >> teclado;
+    switch (teclado)
+    {
+        case 'a':
+            posX-=1;
+            break;
+        case 'd':
+            posX+=1;
+            break;
+        case 'w':
+            posY-=1;
+            break;
+        case 's':
+            posY+=1;
+            break;
+        case 'p':
+            gameOver=true;
+        default:
+            break;
+    }
+    drawMap(posX,posY,map);
+    if (posX<=0) {
+        cout<<"Estas fuera de los limites!, no puedes pasar"<< endl;
+        posX=0;
+        }
+    if (posX>=9) {
+        cout<<"Estas fuera de los limites!, no puedes pasar"<< endl;
+        posX=9;
+         }
+     if (posY<=0) {
+        cout<<"Estas fuera de los limites!, no puedes pasar"<< endl;
+        posY=0;
+        }
+    if (posY>=9) {
+        cout<<"Estas fuera de los limites!, no puedes pasar"<< endl;
+        posY=9;
+        }
+
+
+    }
+    return 0;
+  }
+
+
